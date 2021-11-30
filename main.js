794047684778d73a8130b1912f7cfd57160d9e3a
abdurashiidov <abdurashiidov18@gmail.com>
2021-11-30 08:47:15 +0300

    add change

--- main.js
+++ main.js
@@ -1,22 +1,23 @@
-const minus = document.querySelector(".minus")
-const plus = document.querySelector(".plus")
-const finnally = document.querySelector(".final")
+// const minus = document.querySelector(".minus")
+// const plus = document.querySelector(".plus")
+// const finnally = document.querySelector(".final")
 
-let defaults = 50
-let ozgaruvchi = 50
-minus.disabled = true 
+// let defaults = 50
+// let ozgaruvchi = 50
+// minus.disabled = true 
 
-minus.addEventListener("click", () => {
-    if(ozgaruvchi == defaults){
-        minus.disabled = true
-    } else{
-        ozgaruvchi -= defaults
-    }
-    finnally.textContent = ozgaruvchi
-})
+// minus.addEventListener("click", () => {
+//     if(ozgaruvchi == defaults){
+//         minus.disabled = true
+//     } else{
+//         ozgaruvchi -= defaults
+//     }
+//     finnally.textContent = ozgaruvchi
+// })
 
-plus.addEventListener("click", () => {
-    minus.disabled = false
-    ozgaruvchi += defaults
-    finnally.textContent = ozgaruvchi
-})
+// plus.addEventListener("click", () => {
+//     minus.disabled = false
+//     ozgaruvchi += defaults
+//     finnally.textContent = ozgaruvchi
+// })
+
